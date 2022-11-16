5986bd8f7072ef326f5d46a869d5c858c75b0d88
LBF38 <contact.lbf38@gmail.com>
2022-11-16 20:51:04 +0100

    Exercice 4 - lecture de fichiers

--- makefile
+++ makefile
@@ -1,2 +1,4 @@
 td1: td1.c
 	gcc -Wall -g td1.c -o td1
+td2: td2.c
+	gcc -Wall -g td2.c -o td2
--- td1.c
+++ td1.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
 void ex1()
 {
@@ -92,10 +96,62 @@ void ex3()
     printf("Fin du test fork()\n");
 }
 
+void ex4()
+{
+    int code_retour;
+
+    int file_descriptor = open("toto", O_RDWR);
+    if (file_descriptor == -1)
+    {
+        fprintf(stderr, "Error opening file : %s\n", strerror(errno));
+        exit(-1);
+    }
+    code_retour = fork();
+    switch (code_retour)
+    {
+    case -1:
+        printf("Problème lors de la creation du processus\n");
+        break;
+    case 0:
+        printf("Je suis le processus fils \n");
+        printf("Fils: écriture fichier toto\n");
+        char writeFils[] = "Fils";
+        write(file_descriptor, writeFils, strlen(writeFils));
+        sleep(2);
+        printf("Fils: lecture fichier toto\n");
+        char readTotoFils[100];
+        int size_fils = read(file_descriptor, readTotoFils, 4);
+        if (size_fils == -1)
+        {
+            fprintf(stderr, "Error reading file : %s\n", strerror(errno));
+            exit(-1);
+        }
+        printf("Fils: %s\n", readTotoFils);
+        break;
+    default:
+        printf("Je suis le processus père\n");
+        sleep(1);
+        printf("Père: lecture fichier toto\n");
+        char readTotoPere[100];
+        int size_pere = read(file_descriptor, readTotoPere, 4);
+        if (size_pere == -1)
+        {
+            fprintf(stderr, "Error reading file : %s\n", strerror(errno));
+            exit(-1);
+        }
+        readTotoPere[size_pere] = '\0';
+        printf("Père: %s\n", readTotoPere);
+        printf("Père: écriture fichier toto\n");
+        char writePere[] = "Père";
+        write(file_descriptor, writePere, strlen(writePere));
+    }
+}
+
 int main()
 {
     // ex1();
     // ex2();
-    ex3();
+    // ex3();
+    ex4();
     return 0;
 }
