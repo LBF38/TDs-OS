0a90cadce02e674b3b1f2a0aaa5882dc308113f6
LBF38 <contact.lbf38@gmail.com>
2022-11-25 16:23:49 +0100

    Reorganized the code organisation for TP3

--- TP3/Exercice2/partage.c
+++ TP3/Exercice2/partage.c
@@ -1,11 +1,11 @@
-#include <stdio.h>             /* partage.c */
+#include <stdio.h> /* partage.c */
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
 #include "partage.h"
 
-/*--- 
+/*---
   Pour memoire :
  ==============
 
@@ -13,7 +13,7 @@
    un lien de parente. Exemple: pere-fils
    => Utilisation d'une cle privee d'IPC Sys V
 
-typedef struct 
+typedef struct
 {
  int    id;
  void * debut;
@@ -30,27 +30,34 @@ typedef struct
 
    Remarque : cette zone est heritee par un processus fils
 
-   Retourne : -1 en cas d'erreur, 0 sinon 
+   Retourne : -1 en cas d'erreur, 0 sinon
               remplit la structure zone passee en argument
  --------------------------------------------------- */
 
-int creerZonePartagee(int taille, Zone * zp) 
+int creerZonePartagee(int taille, Zone *zp)
 {
- if (!zp) { fprintf(stderr,"Erreur: zp==NULL dans creerZonePartagee\n");
-            return -1;
- }
+  if (!zp)
+  {
+    fprintf(stderr, "Erreur: zp==NULL dans creerZonePartagee\n");
+    return -1;
+  }
 
- if ((zp->id=shmget(IPC_PRIVATE, taille, 0600|IPC_CREAT))==-1)
-      { perror("shmget (creerZonePartagee)"); return -1; }
+  if ((zp->id = shmget(IPC_PRIVATE, taille, 0600 | IPC_CREAT)) == -1)
+  {
+    perror("shmget (creerZonePartagee)");
+    return -1;
+  }
 
- if ((zp->debut=shmat(zp->id, 0, 0))==(void*)-1)
-      { perror("shmat (creerZonePartagee)"); 
-        shmctl(zp->id, IPC_RMID, NULL);
-        return -1; }
+  if ((zp->debut = shmat(zp->id, 0, 0)) == (void *)-1)
+  {
+    perror("shmat (creerZonePartagee)");
+    shmctl(zp->id, IPC_RMID, NULL);
+    return -1;
+  }
 
- zp->taille=taille;
+  zp->taille = taille;
 
- return 0;
+  return 0;
 }
 
 /* ------------------------------------------------------
@@ -58,20 +65,28 @@ int creerZonePartagee(int taille, Zone * zp)
 
    Libere la zone partagee
 
-   Retourne -1 en cas d'erreur, 0 sinon 
+   Retourne -1 en cas d'erreur, 0 sinon
   ---------------------------------------------------- */
 
-int supprimerZonePartagee(Zone * zp)
+int supprimerZonePartagee(Zone *zp)
 {
- if (!zp) { fprintf(stderr,"Erreur: zp==NULL dans supprimerZonePartagee\n");
-            return -1;
- }
+  if (!zp)
+  {
+    fprintf(stderr, "Erreur: zp==NULL dans supprimerZonePartagee\n");
+    return -1;
+  }
 
- if (shmdt(zp->debut)==-1) 
-     { perror("shmdt (supprimerZonePartagee)"); return -1; }
+  if (shmdt(zp->debut) == -1)
+  {
+    perror("shmdt (supprimerZonePartagee)");
+    return -1;
+  }
 
- if (shmctl(zp->id, IPC_RMID, NULL)==-1) 
-     { perror("shmctl (supprimerZonePartagee)"); return -1; }
+  if (shmctl(zp->id, IPC_RMID, NULL) == -1)
+  {
+    perror("shmctl (supprimerZonePartagee)");
+    return -1;
+  }
 
- return 0;
+  return 0;
 }
--- TP3/Exercice2/partage.h
+++ TP3/Exercice2/partage.h
@@ -1,18 +1,18 @@
-#ifndef PARTAGE_H              /* partage.h */
+#ifndef PARTAGE_H /* partage.h */
 #define PARTAGE_H
 
 /* Structure zone representant une zone partagee entre processus ayant
    un lien de parente. Exemple: pere-fils
    => Utilisation d'une cle privee d'IPC Sys V                         */
-typedef struct 
+typedef struct
 {
- int    id;
- void * debut;
- int    taille;
+   int id;
+   void *debut;
+   int taille;
 } Zone;
 
 /* fonctions publiques */
-extern int creerZonePartagee(int taille, Zone * zp);
-extern int supprimerZonePartagee(Zone * zp);
+extern int creerZonePartagee(int taille, Zone *zp);
+extern int supprimerZonePartagee(Zone *zp);
 
 #endif /* PARTAGE_H */
