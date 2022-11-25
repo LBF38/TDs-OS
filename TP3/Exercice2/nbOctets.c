44168701266d321705ee2dbb88617eee44812b9f
LBF38 <contact.lbf38@gmail.com>
2022-11-25 17:28:35 +0100

    Completed the TP3 instructions comments
    First attempt worked !
    Now, need to verify if everything is ok in all situations.

--- TP3/Exercice2/nbOctets.c
+++ TP3/Exercice2/nbOctets.c
@@ -9,12 +9,13 @@
 
 /* Decrire le handler de signal pour SIGUSR1 */
 /* ========================================= */
-
-/* ... */
+void handler(int signum)
+{
+  printf("Signal recu : %d\n", signum);
+}
 
 /* Le main */
 /* ======= */
-
 int main(int argc, char **argv)
 {
   pid_t pidWC;
@@ -54,38 +55,64 @@ int main(int argc, char **argv)
   /* Preparation de la structure action pour recevoir le signal SIGUSR1 */
 
   /* action.sa_handler = ... */
+  action.sa_handler = handler;
 
   /* Appel a l'appel systeme sigaction */
 
-  /* ... */
+  if (sigaction(SIGUSR1, &action, NULL) == -1)
+  {
+    perror("sigaction");
+    exit(EXIT_FAILURE);
+  }
 
   /* Creation de la zone de memoire partagee */
   /* ======================================= */
-
-  /* ... */
+  creerZonePartagee(sizeof(int), &z);
 
   ptDeb = (int *)z.debut; /* *ptDeb <=> *((int*)z.debut) */
 
   /* Creation du tube */
   /* ================ */
-
-  /* ... */
+  pipe(tube);
 
   /* Creation du processus qui fera le exec ...   */
   /* ============================================ */
 
   /* pidWC=... */
+  pidWC = fork();
+
+  if (pidWC == -1)
+  {
+    perror("pidWC");
+    exit(EXIT_FAILURE);
+  }
 
   /* Dans le processus cree :
      - Rediriger la sortie standard vers le tube
      - Fermer le(s) descripteur(s) inutile(s) a cet enfant
      - Recouvrir par la commande ``wc''
   */
+  if (pidWC == 0)
+  {
+    dup2(tube[1], STDOUT_FILENO); // on redirige la sortie standard vers le tube
+    close(tube[0]);               // on ferme la lecture du tube car non utilisée
+    close(tube[1]);               // on ferme l'écriture du tube car redirigée
+    execlp("wc", "wc", "-c", fileName, NULL);
+    perror("execlp");
+    exit(EXIT_FAILURE);
+  }
 
   /* Creation du processus qui fera la lecture ...*/
   /* ============================================ */
 
   /* pidREAD=... */
+  pidREAD = fork();
+
+  if (pidREAD == -1)
+  {
+    perror("pidREAD");
+    exit(EXIT_FAILURE);
+  }
 
   /* Dans le processus cree :
      - Fermer le(s) descripteur(s) inutile(s) a cet enfant
@@ -95,33 +122,40 @@ int main(int argc, char **argv)
      - Attendre un peu pour que le pere puisse faire pause avant
      - Envoyer le signal SIGUSR1 au pere
   */
+  if (pidREAD == 0)
+  {
+    close(tube[1]); // on ferme l'écriture du tube car non utilisée
+    fIn = fdopen(tube[0], "r");
+    fscanf(fIn, "%d", ptDeb);
+    fclose(fIn);
+    close(tube[0]); // on ferme la lecture du tube car non utilisée
+    sleep(3);
+    kill(getppid(), SIGUSR1);
+    exit(EXIT_SUCCESS);
+  }
 
   /* La suite du pere */
   /* ================ */
 
   /* Fermer les descripteurs de tube inutiles au pere */
-
-  /* ... */
+  // On ferme tous les accès aux tubes car inutile pour le père.
+  close(tube[0]);
+  close(tube[1]);
 
   /* Attente d'un signal */
-
-  /* ... */
+  pause();
 
   /* Recuperer le resultat dans la memoire partagee */
-
-  /* ... */
+  printf("Nombre d'octets : %d", *ptDeb);
 
   /* Attendre le 1er enfant  */
-
-  /* ... */
+  waitpid(pidWC, &status, 0);
 
   /* Attendre le 2eme enfant */
-
-  /* ... */
+  waitpid(pidREAD, &status, 0);
 
   /* Supprimer la memoire partagee */
-
-  /* ... */
+  supprimerZonePartagee(&z);
 
   return 0;
 }
