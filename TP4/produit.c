be35c44a8644beb71dbcc245bd934e7a3e5c3bb1
LBF38 <contact.lbf38@gmail.com>
2022-12-03 11:15:53 +0100

    Work on TP4
    Added documentation of some functions.
    Removed some mutex_unlock in coherence with cond_wait
    Testing the code but not currently working as expected

--- TP4/produit.c
+++ TP4/produit.c
@@ -4,6 +4,17 @@
 #include <stdlib.h>
 
 /*********** Data Type ***********/
+
+/**
+ * @brief States for the operations
+ *
+ * Four states to know what is the current state of the operation
+ * - STATE_WAIT
+ * - STATE_MULT
+ * - STATE_ADD
+ * - STATE_PRINT
+ *
+ */
 typedef enum
 {
     STATE_WAIT,
@@ -12,6 +23,21 @@ typedef enum
     STATE_PRINT
 } State;
 
+/**
+ * @brief Product structure
+ *
+ * Structure shared between the threads to make the operations
+ * - state : current state of the operation
+ * - pendingMult : pointer to an array of the state of multiplication of each thread (0/1)
+ * - cond : condition variable to wait for the state change
+ * - mutex : mutex to protect the structure
+ * - nbIterations : number of iterations to do
+ * - size : size of the array
+ * - v1 : first vector
+ * - v2 : second vector
+ * - v3 : result vector (v1 * v2)
+ * - result : result of the multiplication and addition
+ */
 typedef struct
 {
     State state;
@@ -30,6 +56,14 @@ typedef struct
 Product prod;
 
 /*********** Function ***********/
+
+/**
+ * @brief Initialize the pending multiplication
+ *
+ * Set all multiplication to 1
+ *
+ * @param prod
+ */
 void initPendingMult(Product *prod)
 {
     size_t i;
@@ -39,6 +73,16 @@ void initPendingMult(Product *prod)
     }
 }
 
+/**
+ * @brief Calculate the sum of the pending multiplication
+ *
+ * PendingMult is an array of int. Each element is set to 1 when the multiplication is pending.
+ * When the multiplication is done, the element is set to 0.
+ * Therefore, if all multiplication are done, the sum of the array is 0.
+ *
+ * @param prod
+ * @return int
+ */
 int nbPendingMult(Product *prod)
 {
     size_t i;
@@ -52,11 +96,11 @@ int nbPendingMult(Product *prod)
 
 /**
  * @brief Function to waste time
- * 
+ *
  * This function is used to waste time in order to simulate a long
  * computation.
- * 
- * @param ms 
+ *
+ * @param ms
  */
 void wasteTime(unsigned long ms)
 {
@@ -91,21 +135,20 @@ void *mult(void *data)
         {
             pthread_cond_wait(&prod.cond, &prod.mutex);
         }
-        pthread_mutex_unlock(&prod.mutex);
+        /* Le mutex est verrouillé une fois la condition remplie
+        mais relaché sinon et le thread attend la vérification de la condition.  */
 
         fprintf(stderr, "--> mult(%ld)\n", index); /* La multiplication peut commencer */
 
         /*=>Effectuer la multiplication a l'index du thread courant... */
-        pthread_mutex_lock(&prod.mutex);
         prod.v3[index] = prod.v1[index] * prod.v2[index];
-        pthread_mutex_unlock(&prod.mutex);
 
         wasteTime(200 + (rand() % 200)); /* Perte du temps avec wasteTime() */
 
         /* Affichage du calcul sur l'index */
         fprintf(stderr, "<-- mult(%ld) : %.3g*%.3g=%.3g\n", index, prod.v1[index], prod.v2[index], prod.v3[index]);
+
         /*=>Marquer la fin de la multiplication en cours... */
-        pthread_mutex_lock(&prod.mutex);
         prod.pendingMult[index] = 0;
         pthread_cond_signal(&prod.cond);
         pthread_mutex_unlock(&prod.mutex);
@@ -117,9 +160,7 @@ void *mult(void *data)
             pthread_mutex_lock(&prod.mutex);
             if (nbPendingMult(&prod) == 0)
             {
-                pthread_mutex_lock(&prod.mutex);
                 prod.state = STATE_ADD;
-                pthread_mutex_unlock(&prod.mutex);
             }
             pthread_mutex_unlock(&prod.mutex);
         }
@@ -144,7 +185,6 @@ void *add(void *data)
         {
             pthread_cond_wait(&prod.cond, &prod.mutex);
         }
-        pthread_mutex_unlock(&prod.mutex);
 
         /* L'addition peut commencer */
         fprintf(stderr, "--> add\n");
@@ -162,7 +202,6 @@ void *add(void *data)
         fprintf(stderr, "<-- add\n");
 
         /*=>Autoriser le demarrage de l'affichage... */
-        pthread_mutex_lock(&prod.mutex);
         prod.state = STATE_PRINT;
         pthread_cond_signal(&prod.cond);
         pthread_mutex_unlock(&prod.mutex);
@@ -196,7 +235,6 @@ int main(int argc, char **argv)
 
     /* Initialisations (Product, tableaux, generateur aleatoire,etc) */
     prod.state = STATE_WAIT;
-
     prod.pendingMult = (int *)malloc(prod.size * sizeof(int));
     initPendingMult(&prod);
 
@@ -204,9 +242,7 @@ int main(int argc, char **argv)
     pthread_mutex_init(&prod.mutex, NULL);
 
     /*=>initialiser prod.cond ...  */
-    pthread_mutex_lock(&prod.mutex);
     pthread_cond_init(&prod.cond, NULL);
-    pthread_mutex_unlock(&prod.mutex);
 
     /* Allocation dynamique des 3 vecteurs v1, v2, v3 */
     prod.v1 = (double *)malloc(prod.size * sizeof(double));
@@ -236,9 +272,10 @@ int main(int argc, char **argv)
 
     srand(time((time_t *)0)); /* Init du generateur de nombres aleatoires */
 
-    /* Pour chacune des iterations a realiser, c'est a dire :                   */
-    for (iter = 0; iter < prod.nbIterations; iter++) /* tant que toutes les iterations */
-    {                                                /* n'ont pas eu lieu              */
+    /* Pour chacune des iterations a realiser, c'est a dire :
+       Tant que toutes les itérations n'ont pas eu lieu              */
+    for (iter = 0; iter < prod.nbIterations; iter++)
+    {
         size_t j;
 
         /* Initialiser aleatoirement les deux vecteurs */
@@ -259,10 +296,11 @@ int main(int argc, char **argv)
         {
             pthread_cond_wait(&prod.cond, &prod.mutex);
         }
-        pthread_mutex_unlock(&prod.mutex);
 
         /*=>Afficher le resultat de l'iteration courante...*/
         fprintf(stderr, "Resultat de l'iteration %d : %f\n", (int)iter, prod.result);
+        pthread_cond_signal(&prod.cond);
+        pthread_mutex_unlock(&prod.mutex);
     }
 
     /*=>Attendre la fin des threads de multiplication...*/
@@ -274,13 +312,11 @@ int main(int argc, char **argv)
     /*=>Attendre la fin du thread d'addition...*/
     pthread_join(addTh, &threadReturnValue);
 
-    /*=> detruire prod.cond ... */
-    pthread_mutex_lock(&prod.mutex);
-    pthread_cond_destroy(&prod.cond);
-    pthread_mutex_unlock(&prod.mutex);
-
     /*=> detruire prod.mutex ... */
     pthread_mutex_destroy(&prod.mutex);
+
+    /*=> detruire prod.cond ... */
+    pthread_cond_destroy(&prod.cond);
 
     /* Detruire avec free ce qui a ete initialise avec malloc */
     free(prod.pendingMult);
