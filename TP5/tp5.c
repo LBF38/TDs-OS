56192a7798d7702233af509ba5b86b4710dd85c6
LBF38 <contact.lbf38@gmail.com>
2022-12-09 14:12:18 +0100

    FinEx Exercice 1

--- TP5/tp5.c
+++ TP5/tp5.c
@@ -1,15 +1,24 @@
 #define _GNU_SOURCE
 #include <sched.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc, const char *argv[])
 {
     int cpu = sched_getcpu();
-    if(cpu == -1){
+    if (cpu == -1)
+    {
         perror("sched_getcpu");
         return -1;
     }
-    printf("CPU: %d", cpu);
+
+    while (1)
+    {
+        printf("CPU: %d\n", cpu);
+        sleep(1);
+        cpu = sched_getcpu();
+    }
 
     return 0;
 }
