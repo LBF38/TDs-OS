b205924be9a34d9f82654eaf42ba72db0244fa0c
LBF38 <contact.lbf38@gmail.com>
2022-12-09 14:37:43 +0100

    FinEx Exercice2

--- TP5/tp5.c
+++ TP5/tp5.c
@@ -4,13 +4,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, const char *argv[])
+void ex1()
 {
     int cpu = sched_getcpu();
     if (cpu == -1)
     {
         perror("sched_getcpu");
-        return -1;
+        return;
     }
 
     while (1)
@@ -19,6 +19,30 @@ int main(int argc, const char *argv[])
         sleep(1);
         cpu = sched_getcpu();
     }
+}
 
+void ex2(int cpu)
+{
+    cpu_set_t set;
+    CPU_ZERO(&set);
+    CPU_SET(cpu, &set);
+    if (sched_setaffinity(0, sizeof(set), &set) == -1)
+    {
+        perror("sched_setaffinity");
+        return;
+    }
+    ex1(); // Pour vérifier que le processus est bien sur le bon CPU
+}
+
+int main(int argc, const char *argv[])
+{
+    // ex1();
+    if (argc != 2)
+    {
+        printf("Usage: %s <cpu>\n", argv[0]);
+        return 1;
+    }
+    int cpu = atoi(argv[1]);
+    ex2(cpu);
     return 0;
 }
